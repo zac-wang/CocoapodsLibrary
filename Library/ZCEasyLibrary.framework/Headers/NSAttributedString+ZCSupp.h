aa99a592a65925f25737cbf1ccbc6543e5ea8378
王志超 <love_iphone@qq.com>
2017-11-28 18:45:15 +0800

    NSAttributedString 标签

--- Library/ZCEasyLibrary.framework/Headers/NSAttributedString+ZCSupp.h
+++ Library/ZCEasyLibrary.framework/Headers/NSAttributedString+ZCSupp.h
@@ -24,5 +24,26 @@
                                      tagTextArray:(NSArray *)tagTextArray
                                     paddingString:(NSString *)paddingString
                                      paddingColor:(UIColor *)paddingColor;
+/// 生成换行标签
++ (NSAttributedString *)zc_getTagTextWithTextFont:(UIFont *)font
+                                         backFont:(UIFont *)backFont
+                                        textColor:(UIColor *)textColor
+                                        backColor:(UIColor *)backColor
+                                     tagTextArray:(NSArray *)tagTextArray
+                                    paddingString:(NSString *)paddingString
+                                     paddingColor:(UIColor *)paddingColor
+                                          maxSize:(CGSize)maxSize;
+/// 生成标签数组
++ (NSArray *)zc_getTagsWithTextFont:(UIFont *)font
+                           backFont:(UIFont *)backFont
+                          textColor:(UIColor *)textColor
+                          backColor:(UIColor *)backColor
+                       tagTextArray:(NSArray *)tagTextArray;
+/// 生成单个标签
++ (NSAttributedString *)zc_getTagWithTextFont:(UIFont *)font
+                                     backFont:(UIFont *)backFont
+                                    textColor:(UIColor *)textColor
+                                    backColor:(UIColor *)backColor
+                                      tagText:(NSString *)tagText;
 
 @end
