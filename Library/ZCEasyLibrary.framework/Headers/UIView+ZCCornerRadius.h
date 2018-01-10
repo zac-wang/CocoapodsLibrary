d524ff8160c17c160758a49336e975622b51a444
王志超 <1051215684@qq.com>
2018-01-10 14:08:13 +0800

    update

--- Library/ZCEasyLibrary.framework/Headers/UIView+ZCCornerRadius.h
+++ Library/ZCEasyLibrary.framework/Headers/UIView+ZCCornerRadius.h
@@ -14,7 +14,7 @@
 - (void)zc_drawCornerRadius:(int)radius borderColor:(UIColor *)color;
 - (void)zc_drawCornerRadius:(int)radius borderWidth:(int)width borderColor:(UIColor *)color;
 /**
- *  设置部分圆角(相对布局)
+ *  设置部分圆角
  *
  *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
  *  @param radii   圆角大小
@@ -25,5 +25,20 @@
                                  radii:(CGSize)radii
                            borderColor:(UIColor *)borderColor
                              lineWidth:(float)lineWidth;
+
+/**
+ *  设置部分圆角,可以设置虚线
+ *
+ *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
+ *  @param radii            圆角大小
+ *  @param borderColor      边框线条颜色
+ *  @param lineWidth        边框线条宽度
+ *  @param lineDashPattern  虚线样式
+ */
+- (CAShapeLayer *)zc_addRoundedCorners:(UIRectCorner)corners
+                                 radii:(CGSize)radii
+                           borderColor:(UIColor *)borderColor
+                             lineWidth:(float)lineWidth
+                       lineDashPattern:(NSArray *)lineDashPattern;
 
 @end
