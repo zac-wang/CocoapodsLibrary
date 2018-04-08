05d4b2aff61746ba4ea1d868842107d5b4415aa5
王志超 <1051215684@qq.com>
2018-04-08 19:15:30 +0800

    更新pod

--- Library/ZCEasyLibrary.framework/Headers/ZCTimeCountDown.h
+++ Library/ZCEasyLibrary.framework/Headers/ZCTimeCountDown.h
@@ -1,5 +1,5 @@
 //
-//  ZCSmsCodeManage.h
+//  ZCTimeCountDown.h
 //  ZCEasyLibrary
 //
 //  Created by 王志超 on 2017/12/9.
@@ -8,16 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
-@interface ZCSmsCodeManage : NSObject
+@interface ZCTimeCountDown : NSObject
 
 + (instancetype)shared;
 
-/// 短信发送时间间隔(秒)
+/// 触发时间间隔
 @property (nonatomic, assign) int zc_timeInterval;
-/// 是否可以重新发送短信验证码
-@property (nonatomic, readonly) BOOL zc_isCanPostSmsCode;
-/// 开始计数
+/// 开始倒计时
 - (void)zc_start;
+/// 倒计时是否正在进行中
+@property (nonatomic, readonly) BOOL zc_isStarting;
 /// 更新剩余秒数
 - (void)zc_updateSecond:(void(^)(int second))updateBlock completionHandler:(void(^)(void))completionHandler;
 
