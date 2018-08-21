c3d54963572c1fadabf9770022aaab132a1a67de
王志超 <1051215684@qq.com>
2018-08-21 16:50:25 +0800

    优化

--- Library/ZCEasyLibrary/Category/view/UIView+ZCScreenCapture.m
+++ Library/ZCEasyLibrary/Category/view/UIView+ZCScreenCapture.m
@@ -12,7 +12,7 @@
 
 - (UIImage *)zc_screenCapture {
     //size大小，opaque不透明（YES）,若完全不用透明 YES可以优化位图的存储，scale比例缩放
-    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, self.layer.contentsScale);
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, [UIScreen mainScreen].scale);
     [self.layer renderInContext:UIGraphicsGetCurrentContext()];//当前层渲染到上下文
     UIImage * image = UIGraphicsGetImageFromCurrentImageContext();//上下文形成图片
     UIGraphicsEndImageContext();//结束并删除当前基于位图的图形上下文。
