a14d12f2d216483bff7cb97611848af95e137c19
wzc <wangzhichao-ds@gome.com.cn>
2019-11-09 19:57:08 +0800

    优化代码

--- Library/ZCEasyLibraryIOS/view/PickerView/ZCPickerCirculateView.m
+++ Library/ZCEasyLibraryIOS/view/PickerView/ZCPickerCirculateView.m
@@ -8,20 +8,25 @@
 
 #import "ZCPickerCirculateView.h"
 
+/// 获取源数据长度
+#define OriginalCountForComponent(component) ([super pickerView:self.zc_pickerView numberOfRowsInComponent:component])
+
+/// 源数据扩充倍数
+#define ExpansionMultipleForComponent(component) ({\
+    NSInteger originalCount = OriginalCountForComponent(component);\
+    if(originalCount == 0)\
+        originalCount = 151;\
+    int multiple = MAX(150/originalCount, 1);\
+    multiple = multiple * 2 + 1;\
+})
+
 @interface ZCPickerView() <UIPickerViewDelegate, UIPickerViewDataSource>
 @end
 
 @interface ZCPickerCirculateView()
-///每一列 实际显示数据量 是 原数据数据量 的倍数
-@property(nonatomic, strong) NSMutableDictionary<NSNumber *, NSNumber *> *rowDataMultipleWithComponent;
 @end
 
 @implementation ZCPickerCirculateView
-
-- (void)initView {
-    [super initView];
-    self.rowDataMultipleWithComponent = [[NSMutableDictionary alloc] init];
-}
 
 - (void)didMoveToSuperview {
     [super didMoveToSuperview];
@@ -31,7 +36,7 @@
 #pragma mark - 重写 返回选中的位置
 - (NSInteger(^)(NSInteger))zc_selectRow {
     return ^NSInteger(NSInteger component) {
-        NSInteger originalRowCount = [self originalCount:component];
+        NSInteger originalRowCount = OriginalCountForComponent(component);
         return [self.zc_pickerView selectedRowInComponent:component] % originalRowCount;
     };
 }
@@ -45,38 +50,29 @@
 }
 
 - (void)autoSelect:(NSInteger)component {
-    int multiple = [self.rowDataMultipleWithComponent[@(component)] intValue];
-    NSInteger originalRowCount = [self originalCount:component];
+    int multiple = ExpansionMultipleForComponent(component);
+    NSInteger originalRowCount = OriginalCountForComponent(component);
     NSInteger selectRow = self.zc_selectRow(component);
     
     [self.zc_pickerView selectRow:(multiple/2 + 1) * originalRowCount + selectRow inComponent:component animated:NO];
 }
 
-//获取源数据长度
-- (NSInteger)originalCount:(NSInteger)component {
-    return [super pickerView:self.zc_pickerView numberOfRowsInComponent:component];
-}
-
 #pragma mark - 代理方法
 - (NSInteger)pickerView:(nonnull UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-    NSInteger row = [self originalCount:component];
-    if(row == 0)
+    NSInteger originalCount = OriginalCountForComponent(component);
+    if(originalCount == 0)
         return 0;
     
-    int multiple = 150/row + 1;
-    multiple = multiple * 2 + 1;
-    self.rowDataMultipleWithComponent[@(component)] = @(multiple);
-    
-    return row * multiple;
+    return originalCount * ExpansionMultipleForComponent(component);
 }
 
 - (NSAttributedString *)pickerView:(UIPickerView *)pickerView attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component {
-    NSInteger originalRowCount = [self originalCount:component];
+    NSInteger originalRowCount = OriginalCountForComponent(component);
     return [super pickerView:pickerView attributedTitleForRow:row % originalRowCount forComponent:component];
 }
 
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
-    int multiple = [self.rowDataMultipleWithComponent[@(component)] intValue];
+    int multiple = ExpansionMultipleForComponent(component);
     if(row/[super pickerView:pickerView numberOfRowsInComponent:component] != (multiple/2 + 1)) {
         [self autoSelect:component];
     }
