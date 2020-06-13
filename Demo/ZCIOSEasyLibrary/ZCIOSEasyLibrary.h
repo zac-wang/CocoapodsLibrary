a302a998f452df88fbec04729b7db87ee3a67e13
wzc <love_iphone@qq.com>
2020-06-13 13:46:16 +0800

    创建Library

---
+++ Demo/ZCIOSEasyLibrary/ZCIOSEasyLibrary.h
@@ -0,0 +1,19 @@
+//
+//  ZCIOSEasyLibrary.h
+//  ZCIOSEasyLibrary
+//
+//  Created by wzc on 2020/6/13.
+//  Copyright © 2020 love_iphone@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZCIOSEasyLibrary.
+FOUNDATION_EXPORT double ZCIOSEasyLibraryVersionNumber;
+
+//! Project version string for ZCIOSEasyLibrary.
+FOUNDATION_EXPORT const unsigned char ZCIOSEasyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZCIOSEasyLibrary/PublicHeader.h>
+
+
